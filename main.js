5d3a96a5eed2edbe52957892057e19b792958554
June Choe <jchoe001@gmail.com>
2021-06-30 16:13:17 +0900

    selection instruction italicized

--- main.js
+++ main.js
@@ -4,17 +4,17 @@ Sequence("zzz")
 
 newTrial("zzz",
     newImage("left", "https://i.pinimg.com/originals/2e/05/23/2e0523392db6f1c29c67c1faccbf9d65.jpg")
-        .size(250, 250)
+        .size(200, 200)
     ,
     newImage("right", "https://ukmadcat.com/wp-content/uploads/2019/04/sleepy-cat.jpg")
-        .size(250, 250)
+        .size(200, 200)
     ,
     newCanvas("context", 600, 300)
         .add(0, 0, getImage("left"))
         .add(400, 0, getImage("right"))
         .print()
     ,
-    newText("Select all other fep(s).")
+    newText("<em>Select all other fep(s).</em>")
         .center()
         .print()
     ,

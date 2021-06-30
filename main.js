2bae9fae5dc97c5ed69d1d8d095d94bf4bf42e37
June Choe <jchoe001@gmail.com>
2021-06-30 16:32:46 +0900

    fix png for some imgs

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ newTrial("zzz",
         .center()
         .print()
     ,
-    newImageGrid("as", "pc-cat.jpg pc-dog.jpg")
+    newImageGrid("as", "1.jpg 2.jpg 3.jpg 4.jpg 5.jpg 6.jpg 7.jpg 8.jpg 9.jpg 10.jpg")
         .print()
     ,
     newButton("hello")

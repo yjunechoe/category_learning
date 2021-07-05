6b6921d0ebd3a996a11dd3fffab5f63368b3addd
June Choe <jchoe001@gmail.com>
2021-07-05 22:12:43 +0900

    add single condition

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 PennController.ResetPrefix(null) // Keep here
 
-Sequence("intro", "trial")
+Sequence("intro", "condition-single", "condition-contrast")
 
 newTrial("intro",
     newImage("teacher", "point-none.jpeg")
@@ -23,7 +23,7 @@ newTrial("intro",
         .wait()
 )
 
-newTrial("trial",
+newTrial("condition-contrast",
     newImage("left", "https://i.pinimg.com/originals/2e/05/23/2e0523392db6f1c29c67c1faccbf9d65.jpg")
         .size(200, 200)
     ,
@@ -36,6 +36,48 @@ newTrial("trial",
     newCanvas("train", 600, 300)
         .add(0, 0, getImage("left"))
         .add(400, 0, getImage("right"))
+        .add(240, 50, getImage("point"))
+        .print()
+    ,
+    newText("instruction", "<em>Select all other fep(s).</em>")
+        .center()
+        .print()
+        .hidden()
+    ,
+    newImageGrid("test", "1.jpg 2.jpg 3.jpg 4.jpg 5.png 6.png 7.jpg 8.jpg 9.jpg 10.jpg")
+        .print()
+        .hidden()
+    ,
+    newTimer("interval", 5000)
+        .start()
+        .wait()
+    ,
+    getCanvas("train")
+        .hidden()
+    ,
+    getText("instruction")
+        .visible()
+    ,
+    getImageGrid("test")
+        .visible()
+    ,
+    newButton("continue", "Continue")
+        .print()
+        .wait(getImageGrid("test").test.selectAny())
+    ,
+    getImageGrid("test")
+        .log()
+) 
+
+newTrial("condition-single",
+    newImage("left", "https://i.pinimg.com/originals/2e/05/23/2e0523392db6f1c29c67c1faccbf9d65.jpg")
+        .size(200, 200)
+    ,
+    newImage("point", "point-left.jpg")
+        .size(100, 100)
+    ,
+    newCanvas("train", 600, 300)
+        .add(0, 0, getImage("left"))
         .add(240, 50, getImage("point"))
         .print()
     ,
